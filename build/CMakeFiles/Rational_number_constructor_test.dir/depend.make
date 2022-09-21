54e90ea54201fd2d51db90413dc161f31f99c9b4
Anton Vorobyev <vrbyov12@gmail.com>
2022-09-21 12:52:09 +0300

    Remove excess build files

--- build/CMakeFiles/Rational_number_constructor_test.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Rational_number_constructor_test.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/Rational_number_operators_test.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for Rational_number_operators_test.
